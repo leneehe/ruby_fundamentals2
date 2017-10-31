2a058e05b107cdabf66d07e0fde47e84f207b338
Lena <fatmeow@gmail.com>
2017-10-31 17:37:41 -0400

    Complete exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+def negative?(number)
+  if number <= -1
+    return true
+  end
+    return false
+end
+
+puts negative?(0)
+puts negative?(20/21)
+puts negative?(-51.23)
+puts negative?(2132 - 2134)
+puts negative?(2132 - 2134.55555)
