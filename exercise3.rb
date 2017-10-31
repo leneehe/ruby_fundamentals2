17cad3bae64cd7c92eb3ea1b59417e149e4535d0
Lena <fatmeow@gmail.com>
2017-10-31 17:41:45 -0400

    Complete exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,12 @@
+def is_even?(number)
+  if number % 2 == 0
+    return true
+  end
+    return false
+end
+
+puts is_even?(50)
+puts is_even?(223)
+puts is_even?(6)
+puts is_even?(22.22)
+puts is_even?(-31)
